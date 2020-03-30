ed04d098980830cd9cf11aa4910e5929edb5e691
MichalObi <ndwz123@gmail.com>
2020-03-30 16:17:12 +0200

    Initil fixes for todo list

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <h1 class="ui dividing centered header">Vue.js Todo App</h1>
-    <div class='ui three column centered grid'>
-      <div class='column'>
+    <div class="ui three column centered grid">
+      <div class="column">
         <todo-list v-bind:todos="todos"></todo-list>
         <create-todo v-on:create-todo="createTodo"></create-todo>
       </div>
@@ -11,42 +11,53 @@
 </template>
 
 <script>
-import sweetalert from 'sweetalert';
-import TodoList from './components/TodoList';
-import CreateTodo from './components/CreateTodo';
+  import sweetalert from 'sweetalert';
+  import TodoList from './components/TodoList';
+  import CreateTodo from './components/CreateTodo';
 
-export default {
-  name: 'app',
-  components: {
-    TodoList,
-    CreateTodo,
-  },
-  data() {
-    return {
-      todos: [{
+  const createInitialToDos = () => {
+    const todos = [
+      {
         title: 'Todo A',
         project: 'Project A',
-        done: false,
-      }, {
+        done: true,
+      },
+      {
         title: 'Todo B',
         project: 'Project B',
         done: true,
-      }, {
+      },
+      {
         title: 'Todo C',
         project: 'Project C',
         done: false,
-      }, {
+      },
+      {
         title: 'Todo D',
         project: 'Project D',
         done: false,
-      }],
-    };
-  },
-  methods: {
-    createTodo(newTodo) {
-      this.todos.push(newTodo);
-      sweetalert('Success!', 'To-Do created!', 'success');
+      },
+    ];
+
+    return todos;
+  };
+
+  export default {
+    name: 'app',
+    components: {
+      TodoList,
+      CreateTodo,
     },
-  },
-};
+    data() {
+      return {
+        todos: createInitialToDos(),
+      };
+    },
+    methods: {
+      createTodo(newTodo) {
+        this.todos.push(newTodo);
+        sweetalert('Success!', 'To-Do created!', 'success');
+      },
+    },
+  };
 </script>
